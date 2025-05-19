ab03372dcc4b48079ab2297742f7539df0e9b790
Ofer Mustigman <ofer@cycognito.com>
2025-05-20 02:27:59 +0300

    bugfix

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+rm -rf animations/__pycache__
 PORT=${PORT:-/dev/cu.usbserial-210}
 
 mpremote connect "$PORT" fs mkdir shapes
 mpremote connect "$PORT" fs mkdir animations
-mpremote connect "$PORT" cp shapes/* :shapes
-mpremote connect "$PORT" cp animations/* :animations
+mpremote connect "$PORT" cp shapes/* :shapes/
+mpremote connect "$PORT" cp animations/* :animations/
 mpremote connect "$PORT" cp wifi_consts.py :wifi_consts.py
 mpremote connect "$PORT" cp utils.py :utils.py
 mpremote connect "$PORT" cp wifi_client.py :wifi_client.py
